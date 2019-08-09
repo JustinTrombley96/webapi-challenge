ab7bf8c4d061f871d8c284b6e7889f8d97f77fe0
Justin Trombley <jtrombley96@gmail.com>
2019-08-09 16:19:13 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const server = require('./server.js');
 
-const port = process.env.port || 8000;
+const port = process.env.PORT || 8000;
 
 server.listen(port, () => {
 	console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
